d9867dc97f74aeda52dbf7f915f0ae8e39f7c0a4
fitrianinasir <fitrianinasir8@gmail.com>
2022-09-11 13:41:39 +0700

    fetch data in table example

--- src/Components/MUITable.js
+++ src/Components/MUITable.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -7,60 +7,43 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
+import axios from "axios";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
 
 function MUITable(props) {
+  const [users, setUsers] = useState([]);
   const columns = [
     { id: "name", label: "Name", minWidth: 170 },
-    { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
     {
-      id: "population",
-      label: "Population",
+      id: "email",
+      label: "Email",
       minWidth: 170,
-      align: "right",
-      format: (value) => value.toLocaleString("en-US"),
     },
     {
-      id: "size",
-      label: "Size\u00a0(km\u00b2)",
+      id: "phone_number",
+      label: "Phone Number",
       minWidth: 170,
-      align: "right",
-      format: (value) => value.toLocaleString("en-US"),
-    },
-    {
-      id: "density",
-      label: "Density",
-      minWidth: 170,
-      align: "right",
-      format: (value) => value.toFixed(2),
     },
   ];
 
-  function createData(name, code, population, size) {
-    const density = population / size;
-    return { name, code, population, size, density };
-  }
+  useEffect(() => {
+    getUsers();
+  }, []);
 
-  const rows = [
-    createData("India", "IN", 1324171354, 3287263),
-    createData("China", "CN", 1403500365, 9596961),
-    createData("Italy", "IT", 60483973, 301340),
-    createData("United States", "US", 327167434, 9833520),
-    createData("Canada", "CA", 37602103, 9984670),
-    createData("Australia", "AU", 25475400, 7692024),
-    createData("Germany", "DE", 83019200, 357578),
-    createData("Ireland", "IE", 4857000, 70273),
-    createData("Mexico", "MX", 126577691, 1972550),
-    createData("Japan", "JP", 126317000, 377973),
-    createData("France", "FR", 67022000, 640679),
-    createData("United Kingdom", "GB", 67545757, 242495),
-    createData("Russia", "RU", 146793744, 17098246),
-    createData("Nigeria", "NG", 200962417, 923768),
-    createData("Brazil", "BR", 210147125, 8515767),
-  ];
+  const getUsers = async () => {
+    await axios
+      .get("http://localhost:5000/users")
+      .then((res) => {
+        setUsers(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
 
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
@@ -85,19 +68,15 @@ function MUITable(props) {
                     {column.label}
                   </TableCell>
                 ))}
+                <TableCell>Action</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows
+              {users
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row) => {
                   return (
-                    <TableRow
-                      hover
-                      role="checkbox"
-                      tabIndex={-1}
-                      key={row.code}
-                    >
+                    <TableRow hover role="checkbox" tabIndex={-1} key={row._id}>
                       {columns.map((column) => {
                         const value = row[column.id];
                         return (
@@ -108,6 +87,30 @@ function MUITable(props) {
                           </TableCell>
                         );
                       })}
+                      <TableCell>
+                        <Stack spacing={2} direction="row">
+                          <Button
+                            variant="outlined"
+                            onClick={() => alert(`edited ${row._id}`)}
+                          >
+                            <Typography
+                              variant="overline"
+                            >
+                              Edit
+                            </Typography>
+                          </Button>
+                          <Button
+                            variant="outlined"
+                            onClick={() => alert(`deleted ${row._id}`)}
+                          >
+                            <Typography
+                              variant="overline"
+                            >
+                              Delete
+                            </Typography>
+                          </Button>
+                        </Stack>
+                      </TableCell>
                     </TableRow>
                   );
                 })}
@@ -117,7 +120,7 @@ function MUITable(props) {
         <TablePagination
           rowsPerPageOptions={[3, 5, 10]}
           component="div"
-          count={rows.length}
+          count={users.length}
           rowsPerPage={rowsPerPage}
           page={page}
           onPageChange={handleChangePage}
